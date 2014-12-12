e86c2f6738f360a884dbdb78bcf61b043cb1423d
Swapnil nandurkar <swapnilnandurkar.gec@gmail.com>
2014-12-12 13:26:23 +0530

    Initial Commit

---
+++ Employee Project/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Employee Project
+//
+//  Created by Ashish limje on 12/12/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Employee ProjectTests/Employee_ProjectTests.m
@@ -0,0 +1,34 @@
+//
+//  Employee_ProjectTests.m
+//  Employee ProjectTests
+//
+//  Created by Ashish limje on 12/12/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Employee_ProjectTests : XCTestCase
+
+@end
+
+@implementation Employee_ProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
